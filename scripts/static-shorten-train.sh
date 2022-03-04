c710927e811128d0f2799bcf6d995418cac7bb8e
Olivia Weng <oliviajweng@gmail.com>
2022-03-04 13:41:19 -0800

    update script for k8s static skip shorten training

--- scripts/static-shorten-train.sh
+++ scripts/static-shorten-train.sh
@@ -1,4 +1,4 @@
-CMD="bonito train ./training/static-skip-shorten --no-amp --config=./bonito/models/configs/dna_r9.4.1@v1-static-skip-shorten.toml --directory ./bonito/data/dna_r9.4.1/ --batch=64"
+CMD="bonito train ./training/static-skip-shorten --no-amp --config=./bonito/models/configs/dna_r9.4.1@v1-static-skip-shorten.toml --directory ./bonito/data/dna_r9.4.1/ --batch=64 -f"
 
 echo $CMD
 $CMD
