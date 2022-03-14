b31181432d74f14101e6cbe3033680698b285b95
Olivia Weng <oliviajweng@gmail.com>
2022-03-14 16:59:32 -0700

    add script for dynamic skip shortening

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,4 +1,4 @@
-CMD="bonito train ./testing/dynamic-skip-remove --no-amp --config=./bonito/models/configs/dna_r9.4.1@v1.toml --directory ./bonito/data/dna_r9.4.1/ --batch=8 --teacher=./training/baseline -f --modifier=remove --testing"
+CMD="bonito train ./testing/dynamic-skip-shorten --no-amp --config=./bonito/models/configs/dna_r9.4.1@v1-dynamic-skip-shorten.toml --directory ./bonito/data/dna_r9.4.1/ --batch=8 --teacher=./training/baseline -f --modifier=shorten --testing"
 
 echo $CMD
 $CMD
