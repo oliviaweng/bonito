ca826826003ba2ce4b62d3c1fd601d2900caf41d
Olivia Weng <oliviajweng@gmail.com>
2022-03-14 17:16:13 -0700

    reduce batch size for dynamic skip shortening to run on 2080

--- scripts/dynamic-skip-shorten-train.sh
+++ scripts/dynamic-skip-shorten-train.sh
@@ -1,4 +1,4 @@
-CMD="bonito train ./training/dynamic-skip-shorten --no-amp --config=./bonito/models/configs/dna_r9.4.1@v1-dynamic-skip-shorten.toml --directory ./bonito/data/dna_r9.4.1/ --batch=32 --teacher=./training/baseline --modifier=shorten -f"
+CMD="bonito train ./training/dynamic-skip-shorten --no-amp --config=./bonito/models/configs/dna_r9.4.1@v1-dynamic-skip-shorten.toml --directory ./bonito/data/dna_r9.4.1/ --batch=24 --teacher=./training/baseline --modifier=shorten -f"
 
 echo $CMD
 $CMD
