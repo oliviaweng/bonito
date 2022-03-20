a9c6eeb8a0368c4e236dc5c7597676055d44bd3a
Olivia Weng <oliviajweng@gmail.com>
2022-03-20 09:32:18 -0700

    remove testing flag

--- scripts/dynamic-skip-shorten-train.sh
+++ scripts/dynamic-skip-shorten-train.sh
@@ -1,4 +1,4 @@
-CMD="bonito train ./training/dynamic-skip-shorten-pretrained-student --no-amp --config=./bonito/models/configs/dna_r9.4.1@v1-dynamic-skip-shorten.toml --directory ./bonito/data/dna_r9.4.1/ --batch=16 --teacher=./training/skip-shorten-teacher --modifier=shorten -f --testing"
+CMD="bonito train ./training/dynamic-skip-shorten-pretrained-student --no-amp --config=./bonito/models/configs/dna_r9.4.1@v1-dynamic-skip-shorten.toml --directory ./bonito/data/dna_r9.4.1/ --batch=16 --teacher=./training/skip-shorten-teacher --modifier=shorten -f"
 
 echo $CMD
 $CMD
