1966a4d59498831761ff0fed581eca29e3d41ff7
Olivia Weng <oliviajweng@gmail.com>
2022-04-05 15:05:11 -0700

    update scripts to restore optimizer state in addition to weights to resume training from checkpoint

--- scripts/dyn-skip-shorten-10epochs.sh
+++ scripts/dyn-skip-shorten-10epochs.sh
@@ -1,4 +1,4 @@
-CMD="bonito train ./training/dynamic-skip-shorten-pretrained-student-10epochs --no-amp --config=./bonito/models/configs/dna_r9.4.1@v1-dynamic-skip-shorten.toml --directory ./bonito/data/dna_r9.4.1/ --batch=16 --teacher=./training/skip-shorten-teacher --epochs=4 --modifier=shorten --modifier-freq=1 -f"
+CMD="bonito train ./training/dynamic-skip-shorten-pretrained-student-10epochs --no-amp --config=./bonito/models/configs/dna_r9.4.1@v1-dynamic-skip-shorten.toml --directory ./bonito/data/dna_r9.4.1/ --batch=16 --teacher=./training/skip-shorten-teacher --epochs=4 --modifier=shorten --modifier-freq=1 --restore-optim -f"
 
 echo $CMD
 $CMD
--- scripts/dyn-skip-shorten-every2.sh
+++ scripts/dyn-skip-shorten-every2.sh
@@ -1,4 +1,4 @@
-CMD="bonito train ./training/dynamic-skip-shorten-pretrained-student-every2epochs --no-amp --config=./bonito/models/configs/dna_r9.4.1@v1-dynamic-skip-shorten.toml --directory ./bonito/data/dna_r9.4.1/ --batch=16 --teacher=./training/skip-shorten-teacher --epochs=8 --modifier=shorten --modifier-freq=2 -f"
+CMD="bonito train ./training/dynamic-skip-shorten-pretrained-student-every2epochs --no-amp --config=./bonito/models/configs/dna_r9.4.1@v1-dynamic-skip-shorten.toml --directory ./bonito/data/dna_r9.4.1/ --batch=16 --teacher=./training/skip-shorten-teacher --epochs=8 --modifier=shorten --modifier-freq=2 --restore-optim -f"
 
 echo $CMD
 $CMD
